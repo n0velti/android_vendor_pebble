bf77d29583ddccfeefcc05c46d32e91d4f812031
edward <eanui042@uottawa.ca>
2021-01-04 12:58:25 -0500

    added pebble_x86_64-eng

--- build/target/product/pebble_x86_64.mk
+++ build/target/product/pebble_x86_64.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,18 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_arm.mk \
-    $(LOCAL_DIR)/lineage_arm64.mk \
-    $(LOCAL_DIR)/lineage_x86.mk \
-    $(LOCAL_DIR)/lineage_x86_64.mk \
-    $(LOCAL_DIR)/pebble_x86_64.mk 
+$(call inherit-product, build/target/product/aosp_x86_64.mk)
 
-COMMON_LUNCH_CHOICES := \
-    lineage_arm-userdebug \
-    lineage_arm64-userdebug \
-    lineage_x86-userdebug \
-    lineage_x86_64-userdebug \
-    pebble_x86_64-eng
+include vendor/pebble/build/target/product/lineage_generic_target.mk
+
+PRODUCT_NAME := pebble_x86_64
